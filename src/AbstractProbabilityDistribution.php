4e01312c3e749d346eb284822bc53499744777be
Michael Cordingley (mcordingley) <Michael.Cordingley@gmail.com>
2015-03-06 12:49:43 -0500

    Document up some functions.

--- src/AbstractProbabilityDistribution.php
+++ src/AbstractProbabilityDistribution.php
@@ -4,10 +4,41 @@ namespace mcordingley\ProbabilityDistribution;
 
 abstract class AbstractProbabilityDistribution
 {
+    /**
+     * getPdf
+     * 
+     * In the case of a discrete distribution, tells you the probability of
+     * getting exactly the stated value. In the case of a continuous
+     * distribution, this value is the simply the derivative of the CDF.
+     * 
+     * @param float $x
+     * @return float
+     */
     abstract public function getPdf($x);
 
+    /**
+     * getCdf
+     * 
+     * Tells you the probability of getting the test statistic or anything less.
+     * 
+     * @param float $x
+     * @return float
+     */
     abstract public function getCdf($x);
     
+    /**
+     * getSf
+     * 
+     * Also known as the "Complementary Cumulative Distribution Function" or as
+     * the "Reliability Function". Whereas the CDF tells you the probability of
+     * getting the test statistic or anything less, this tells you the
+     * probability of getting a value more extreme than the test statistic.
+     * 
+     * If you're looking to do a one-tailed test, this is your function.
+     * 
+     * @param float $x
+     * @return float
+     */
     public function getSf($x)
     {
         return 1 - $this->getCdf($x);
