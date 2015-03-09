3ae319b106fa187f6d0dcdb15fbf9def42f21974
Michael Cordingley <Michael.Cordingley@gmail.com>
2015-03-09 18:09:47 -0400

    Add Pareto distribution.

---
+++ src/Continuous/Pareto.php
@@ -0,0 +1,94 @@
+<?php
+ 
+namespace mcordingley\ProbabilityDistribution\Continuous;
+
+/**
+ * Pareto
+ * 
+ * Also known as the Bradford distribution, this is a power-law distribution.
+ * It is frequently used to model the distribution of wealth, popularity,
+ * population, etc.
+ * 
+ * For more information, see: http://en.wikipedia.org/wiki/Pareto_distribution
+ */
+class Pareto extends AbstractContinuousProbabilityDistribution
+{
+	protected $minimum;
+	protected $alpha;
+	
+	/**
+	 * __construct
+	 * 
+	 * @param float $minimum The minimum value that the distribution can take on
+	 * @param float $alpha The shape parameter
+	 */
+	public function __construct($minimum = 1.0, $alpha = 1.0)
+    {
+		$this->minimum = $minimum;
+		$this->alpha = $alpha;
+	}
+	
+	public function getPdf($x)
+    {
+		if ($x >= $this->minimum) {
+            return $this->alpha * pow($this->minimum, $this->alpha) / pow($x, $this->alpha + 1);
+        } else {
+            return 0.0;
+        }
+	}
+	
+	public function getCdf($x)
+    {
+		if ($x >= $this->minimum) {
+            return 1 - pow($this->minimum / $x, $this->alpha);
+        } else {
+            return 0.0;
+        }
+	}
+	
+	public function getPpf($x)
+    {
+		return $this->minimum / pow(1 - $x, 1 / $this->alpha);
+	}
+    
+    public function getMean()
+    {
+        if ($this->alpha > 1) {
+            return ($this->alpha * $this->minimum) / ($this->alpha - 1);
+        } else {
+            return NAN;
+        }
+    }
+    
+    public function getVariance()
+    {
+        if ($this->alpha > 2) {
+            return (pow($this->minimum, 2) * $this->alpha) / (pow($this->alpha - 1, 2) * ($this->alpha - 2));
+        } else {
+            return NAN;
+        }
+    }
+    
+    public function getSkew()
+    {
+        if ($this->alpha > 3) {
+            return ((2 + 2 * $this->alpha) / ($this->alpha - 3)) * sqrt(($this->alpha - 2) / $this->alpha);
+        } else {
+            return NAN;
+        }
+    }
+
+    public function getKurtosis()
+    {
+        if ($this->alpha > 4) {
+            return (6 * (pow($this->alpha, 3) + pow($this->alpha, 2) - 6 * $this->alpha - 2)) / ($this->alpha * ($this->alpha - 3) * ($this->alpha - 4));
+        } else {
+            return NAN;
+        }
+	}
+	
+	public function generateRandomVariate()
+    {
+		return $this->minimum / pow(mt_rand() / mt_getrandmax(), 1 / $this->alpha);
+	}
+}
---
+++ tests/Continuous/ParetoTest.php
@@ -0,0 +1,74 @@
+<?php
+
+use mcordingley\ProbabilityDistribution\Continuous\Pareto;
+
+class ParetoTest extends PHPUnit_Framework_TestCase
+{
+	private $testObject;
+
+	public function __construct()
+    {
+		$this->testObject = new Pareto(1, 5);
+	}
+
+	public function test_pdf()
+    {
+		$this->assertEquals(1.67449, round($this->testObject->getPdf(1.2), 5));
+		$this->assertEquals(0.29802, round($this->testObject->getPdf(1.6), 5));
+	}
+
+	public function test_cdf()
+    {
+		$this->assertEquals(0.5, round($this->testObject->getCdf(1.1487), 3));
+		$this->assertEquals(0.9, round($this->testObject->getCdf(1.58489), 3));
+	}
+
+	public function test_sf()
+    {
+		$this->assertEquals(0.5, round($this->testObject->getSf(1.1487), 3));
+		$this->assertEquals(0.1, round($this->testObject->getSf(1.58489), 3));
+	}
+
+	public function test_ppf()
+    {
+		$this->assertEquals(1.1487, round($this->testObject->getPpf(0.5), 4));
+		$this->assertEquals(1.58489, round($this->testObject->getPpf(0.9), 5));
+	}
+
+	public function test_isf()
+    {
+		$this->assertEquals(1.1487, round($this->testObject->getIsf(0.5), 4));
+		$this->assertEquals(1.58489, round($this->testObject->getIsf(0.1), 5));
+	}
+    
+    public function test_mean()
+    {
+		$this->assertEquals(1.25, round($this->testObject->getMean(), 2));
+    }
+    
+    public function test_variance()
+    {
+		$this->assertEquals(0.10417, round($this->testObject->getVariance(), 5));
+    }
+    
+    public function test_skew()
+    {
+		$this->assertEquals(4.64758, round($this->testObject->getSkew(), 5));
+    }
+
+	public function test_stats()
+    {
+		$this->assertEquals(70.8, round($this->testObject->getKurtosis(), 1));
+	}
+
+    /*
+     * Depends on the Kolmogorov-Smirnov test.
+	public function test_rvs()
+    {
+		$variates = array();
+		for ($i = 0; $i < 10000; $i++) $variates[] = $this->testObject->rvs();
+		$this->assertGreaterThanOrEqual(0.01, \PHPStats\StatisticalTests::kolmogorovSmirnov($variates, $this->testObject));
+		$this->assertLessThanOrEqual(0.99, \PHPStats\StatisticalTests::kolmogorovSmirnov($variates, $this->testObject));
+	}
+    */
+}
